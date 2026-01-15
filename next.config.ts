53ae3de6fee378fc158cc8cf8738e14d81ef0c34
Alejandro Canizares <63413161+alejocv7@users.noreply.github.com>
2026-01-14 21:10:28 -0500

    Add static setting for nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // required for GitHub Pages
+  },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
